23844c964737893f0e438696fd1e87f6086daf5e
morningdip <morningdip@hotmail.com>
2024-06-25 22:08:22 +0800

    add: init script

---
+++ .init.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+git clone --bare git@github.com:morningdip/dotfiles.git $HOME/.dotfiles
+
+# Define config alias locally since the dotfiles aren't installed on the system yet
+function dots {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+# Create a directory to backup existing dotfiles to
+mkdir -p $HOME/.dotfiles-backup
+dots checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles from git@github.com:morningdip/dotfiles.git";
+  else
+    echo "Moving existing dotfiles to ~/.dotfiles-backup";
+    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backup/{}
+fi
+
+# Checkout dotfiles from repo
+dots checkout
+dots config status.showUntrackedFiles no
